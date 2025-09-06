44b8a1f5d40b65aaaca13c7af9a5cd617304dd20
Elberth Luque <elberthluquechavez@gmail.com>
2025-09-05 19:45:45 -0500

    eltony2412/carrusel

--- src/login/login.js
+++ src/login/login.js
@@ -6,7 +6,7 @@ import Carrusel from "../componentes/carrrusel"
     "https://i.pinimg.com/originals/51/82/ac/5182ac536727d576c78a9320ac62de30.jpg",
     "https://wallpapercave.com/wp/wp3386769.jpg",
     "https://wallpaperaccess.com/full/809523.jpg",
-    "https://getwallpapers.com/wallpaper/full/5/c/0/606489.jpg",
+
   ];
 
 
